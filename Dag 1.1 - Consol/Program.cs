8e1c5f63f30d957ba9c7c0ece50dfb0ccd26d9e1
OPGCoding <97985921+OPGCoding@users.noreply.github.com>
2024-02-19 11:09:13 +0100

    Update Program.cs

--- Dag 1.1 - Consol/Program.cs
+++ Dag 1.1 - Consol/Program.cs
@@ -75,10 +75,24 @@ int widgetsSold = 7;
 Console.WriteLine(firstName + " sold " + widgetsSold + " widgets.");
 Console.WriteLine(firstName + " sold " + widgetsSold + 7 + " widgets.");
 
+int sum = 7 + 5;
+int difference = 7 - 5;
+int product = 7 * 5;
+int quotient = 7 / 5;
 
+Console.WriteLine("Sum: " + sum);
+Console.WriteLine("Difference: " + difference);
+Console.WriteLine("Product: " + product);
+Console.WriteLine("Quotient: " + quotient);
+decimal decimalQuotient = 7.0m / 5;
+Console.WriteLine($"Decimal quotient: {decimalQuotient}");
+int first = 7;
+int second = 5;
+decimal quotient2 = (decimal)first / (decimal)second;
+Console.WriteLine(quotient);
 
-
-
+Console.WriteLine($"Modulus of 200 / 5 : {200 % 5}");
+Console.WriteLine($"Modulus of 7 / 5 : {7 % 5}");
 
 
 
