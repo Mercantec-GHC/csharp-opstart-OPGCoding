85d53eb833c6000151f7782731049611b0dd1aab
OPGCoding <97985921+OPGCoding@users.noreply.github.com>
2024-02-19 10:55:02 +0100

    Update Program.cs

--- Dag 1.1 - Consol/Program.cs
+++ Dag 1.1 - Consol/Program.cs
@@ -39,7 +39,9 @@ Console.WriteLine(@"c:\invoices");
 Console.WriteLine("\u3053\u3093\u306B\u3061\u306F World!");
 Console.Write("\n\n\u65e5\u672c\u306e\u8acb\u6c42\u66f8\u3092\u751f\u6210\u3059\u308b\u306b\u306f\uff1a\n\t");
 Console.WriteLine(@"c:\invoices\app.exe -j");
-
+string firstname1 = "Benjamin";
+string message1 = "Hello " + firstname1;
+Console.WriteLine(message);
 
 
 
