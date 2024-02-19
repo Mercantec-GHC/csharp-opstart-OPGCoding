3f2348f8b826ab7f08c65d8b4bc48017c4ef0541
OPGCoding <97985921+OPGCoding@users.noreply.github.com>
2024-02-19 10:29:49 +0100

    Update Program.cs

--- Dag 1.1 - Consol/Program.cs
+++ Dag 1.1 - Consol/Program.cs
@@ -27,3 +27,5 @@ Console.WriteLine("Hello, " + name + "! " + "You have " + number + " messages in
 Console.WriteLine("Hello World!");
 Console.WriteLine("Hello\nWorld!");
 Console.WriteLine("Hello\tWorld!");
+Console.WriteLine("Hello \"World\"!");
+Console.WriteLine("c:\\source\\repos");
