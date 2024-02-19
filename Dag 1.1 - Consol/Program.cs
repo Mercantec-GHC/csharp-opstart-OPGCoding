9eb635417f490b834b4285655ef6731d40534404
OPGCoding <97985921+OPGCoding@users.noreply.github.com>
2024-02-19 10:06:39 +0100

    Update Program.cs

--- Dag 1.1 - Consol/Program.cs
+++ Dag 1.1 - Consol/Program.cs
@@ -3,3 +3,12 @@ Console.WriteLine(2.625);
 Console.WriteLine(12.39816m);
 Console.WriteLine(true);
 Console.WriteLine(false);
+/* 
+string firstName;
+char userOptions;
+int gameScore;
+decimal particlesPerMillion;
+bool processedCustomer;
+*/
+string firstName;
+firstName = "Benjamin";
