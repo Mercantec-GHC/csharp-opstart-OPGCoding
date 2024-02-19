ca3f7075a8df77b74bf07de94e23e66f6642d0f0
OPGCoding <97985921+OPGCoding@users.noreply.github.com>
2024-02-19 10:53:58 +0100

    Update Program.cs

--- Dag 1.1 - Consol/Program.cs
+++ Dag 1.1 - Consol/Program.cs
@@ -35,3 +35,14 @@ Console.WriteLine("Invoice: 1022\t\tComplete!");
 Console.WriteLine("\nOutput Directory:\t");
 Console.WriteLine(@"    c:\source\repos
         (this is where your code goes)");
+Console.WriteLine(@"c:\invoices");
+Console.WriteLine("\u3053\u3093\u306B\u3061\u306F World!");
+Console.Write("\n\n\u65e5\u672c\u306e\u8acb\u6c42\u66f8\u3092\u751f\u6210\u3059\u308b\u306b\u306f\uff1a\n\t");
+Console.WriteLine(@"c:\invoices\app.exe -j");
+
+
+
+
+
+
+
