313bf6e635b3ae8fcaf79c0762744e061d1ae2a9
OPGCoding <97985921+OPGCoding@users.noreply.github.com>
2024-02-20 11:19:52 +0100

    Updated code

--- Dag 2.1 - ConsolApp/Program.cs
+++ Dag 2.1 - ConsolApp/Program.cs
@@ -1,18 +1,24 @@
-﻿string str = "The quick brown fox jumps over the lazy dog.";
+﻿string originalMessage = "The quick brown fox jumps over the lazy dog.";
 
 // convert the message into a char array
-char[] charMessage = str.ToCharArray();
+char[] message = originalMessage.ToCharArray();
 
 // Reverse the chars
-Array.Reverse(charMessage);
-int x = 0;
+Array.Reverse(message);
+int letterCount = 0;
 
 // count the o's
-foreach (char i in charMessage) { if (i == 'o') { x++; } }
+foreach (char i in message) 
+{ 
+    if (i == 'o') 
+    { 
+        letterCount++; 
+    } 
+}
 
 // convert it back to a string
-string new_message = new String(charMessage);
+string new_message = new String(message);
 
 // print it out
 Console.WriteLine(new_message);
-Console.WriteLine($"'o' appears {x} times.");
+Console.WriteLine($"'o' appears {letterCount} times.");
