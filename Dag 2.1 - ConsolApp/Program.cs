0af811d5d6b8544a475f5d94ed8b5454d945e4d3
OPGCoding <97985921+OPGCoding@users.noreply.github.com>
2024-02-20 09:34:39 +0100

    Implemented example of Contains in string

--- Dag 2.1 - ConsolApp/Program.cs
+++ Dag 2.1 - ConsolApp/Program.cs
@@ -17,3 +17,12 @@ else
 {
     Console.WriteLine("You lost, you owe me a lollipop");
 }
+
+string message = "The quick brown fox jumps over the lazy dog.";
+bool result = message.Contains("dog");
+Console.WriteLine(result);
+
+if (message.Contains("fox"))
+{
+    Console.WriteLine("What does the fox say?");
+}
