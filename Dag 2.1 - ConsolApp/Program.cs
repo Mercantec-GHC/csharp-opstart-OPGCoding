012eda65b619c7fc1a6a2952d2508e9c15231e83
OPGCoding <97985921+OPGCoding@users.noreply.github.com>
2024-02-20 11:12:41 +0100

    Comments

--- Dag 2.1 - ConsolApp/Program.cs
+++ Dag 2.1 - ConsolApp/Program.cs
@@ -1,51 +1,22 @@
 ﻿/*
-string[] fraudulentOrderIDs = new string[3];
-
-fraudulentOrderIDs[0] = "A123";
-fraudulentOrderIDs[1] = "B456";
-fraudulentOrderIDs[2] = "C789";
-// fraudulentOrderIDs[3] = "D000";
-
-
-string[] fraudulentOrderIDs = { "A123", "B456", "C789" };
-
-Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
-Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
-Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
-
-fraudulentOrderIDs[0] = "F000";
-
-Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
-
-Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to process.");
-
-
-string[] names = { "Benjamin", "Kvist", "Stine", "Mathias" };
-foreach(string name in names)
-{
-    Console.WriteLine(name);
-}
-
-Console.WriteLine("\n");
-
-int[] inventory = { 200, 450, 700, 175, 250 };
-int sum = 0;
-int bin = 0;
-foreach (int items in inventory)
-{
-    sum += items;
-    bin++;
-    Console.WriteLine($"Bin {bin} = {items} items (Running total: {sum})");
-}
-
-Console.WriteLine($"We have {sum} items in inventory.");
+  The following code creates five random OrderIDs
+  to test the fraud detection process.  OrderIDs 
+  consist of a letter from A to E, and a three
+  digit number. Ex. A123.
 */
+Random random = new Random();
+string[] orderIDs = new string[5];
 
-string[] orderID = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
-foreach(string id in orderID)
+for (int i = 0; i < orderIDs.Length; i++)
 {
-    if (id.StartsWith("B"))
-    {
-        Console.WriteLine($"{id}");
-    }
+    int prefixValue = random.Next(65, 70);
+    string prefix = Convert.ToChar(prefixValue).ToString();
+    string suffix = random.Next(1, 1000).ToString("000");
+
+    orderIDs[i] = prefix + suffix;
+}
+
+foreach (var orderID in orderIDs)
+{
+    Console.WriteLine(orderID);
 }
