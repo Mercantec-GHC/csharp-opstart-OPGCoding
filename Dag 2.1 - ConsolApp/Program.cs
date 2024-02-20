449a4fc24d7f7ca1d7c2c75fbb31819971759432
OPGCoding <97985921+OPGCoding@users.noreply.github.com>
2024-02-20 11:15:55 +0100

    More whitespaces

--- Dag 2.1 - ConsolApp/Program.cs
+++ Dag 2.1 - ConsolApp/Program.cs
@@ -1,23 +1,18 @@
-﻿Random dice = new Random();
+﻿string str = "The quick brown fox jumps over the lazy dog.";
 
-int roll1 = dice.Next(1, 7);
-int roll2 = dice.Next(1, 7);
-int roll3 = dice.Next(1, 7);
+// convert the message into a char array
+char[] charMessage = str.ToCharArray();
 
-int total = roll1 + roll2 + roll3;
+// Reverse the chars
+Array.Reverse(charMessage);
+int x = 0;
 
-Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+// count the o's
+foreach (char i in charMessage) { if (i == 'o') { x++; } }
 
-if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
-{
-    if ((roll1 == roll2) && (roll2 == roll3))
-    {
-        Console.WriteLine("You rolled triples!  +6 bonus to total!");
-        total += 6;
-    }
-    else
-    {
-        Console.WriteLine("You rolled doubles!  +2 bonus to total!");
-        total += 2;
-    }
-}
+// convert it back to a string
+string new_message = new String(charMessage);
+
+// print it out
+Console.WriteLine(new_message);
+Console.WriteLine($"'o' appears {x} times.");
