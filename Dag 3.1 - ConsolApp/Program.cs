46fc3d1bc74f2942b8bee2ede0718219a747904c
OPGCoding <97985921+OPGCoding@users.noreply.github.com>
2024-02-21 09:28:54 +0100

    Head or tails

--- Dag 3.1 - ConsolApp/Program.cs
+++ Dag 3.1 - ConsolApp/Program.cs
@@ -4,3 +4,16 @@
 int saleAmount = 1001;
 int discount = saleAmount > 1000 ? 100 : 50;
 Console.WriteLine($"Discount: {discount}");
+
+
+Random HeadOrTail = new Random();
+
+for (int i = 0; i < 10; i++)
+{
+    int headOrTail = HeadOrTail.Next(0, 2);
+
+    string coinFlip = headOrTail == 0 ? "Head" : "Tail";
+
+    Console.WriteLine(coinFlip);
+
+}
