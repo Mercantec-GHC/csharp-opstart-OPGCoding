313699d28e19979ff942f472e4a670b23ef8742f
OPGCoding <97985921+OPGCoding@users.noreply.github.com>
2024-02-21 10:29:19 +0100

    Flag task

--- Dag 3.1 - ConsolApp/Program.cs
+++ Dag 3.1 - ConsolApp/Program.cs
@@ -1,24 +1,33 @@
-﻿// string permission = "Manager";
-string permission = "Admin";
-int level = 55;
+﻿//// string permission = "Manager";
+//string permission = "Admin";
+//int level = 55;
 
-if (permission.Contains("Admin") && level > 55)
+//if (permission.Contains("Admin") && level > 55)
+//{
+//    Console.WriteLine("Welcome, Super Admin user.");
+//}
+//else if (permission.Contains("Admin") && level <= 55)
+//{
+//    Console.WriteLine("Welcome, Admin user.");
+//}
+//else if (permission.Contains("Manager") && level >= 20)
+//{
+//    Console.WriteLine("Contact an Admin for access.");
+//}
+//else if (permission.Contains("Manager") && level < 20)
+//{
+//    Console.WriteLine("You do not have sufficient privileges.");
+//}
+//else
+//{
+//    Console.WriteLine("You do not have sufficient privileges.");
+//}
+
+
+
+bool flag = true;
+if (flag)
 {
-    Console.WriteLine("Welcome, Super Admin user.");
-}
-else if (permission.Contains("Admin") && level <= 55)
-{
-    Console.WriteLine("Welcome, Admin user.");
-}
-else if (permission.Contains("Manager") && level >= 20)
-{
-    Console.WriteLine("Contact an Admin for access.");
-}
-else if (permission.Contains("Manager") && level < 20)
-{
-    Console.WriteLine("You do not have sufficient privileges.");
-}
-else
-{
-    Console.WriteLine("You do not have sufficient privileges.");
+    int value = 10;
+    Console.WriteLine($"Inside the code block: {value}");
 }
