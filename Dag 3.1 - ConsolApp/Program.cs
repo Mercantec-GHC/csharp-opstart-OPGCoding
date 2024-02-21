73974a66c6ce2460cecb3d27a9d3ae468ca99816
OPGCoding <97985921+OPGCoding@users.noreply.github.com>
2024-02-21 10:59:39 +0100

    Code challenge: rewrite if-elseif-else using a switch statement done

--- Dag 3.1 - ConsolApp/Program.cs
+++ Dag 3.1 - ConsolApp/Program.cs
@@ -35,22 +35,78 @@
 //Console.WriteLine($"Outside the code block: {value}");
 
 
-using System;
+//using System;
 
-int[] numbers = { 4, 8, 15, 16, 23, 42 };
-int total = 0;
+//int[] numbers = { 4, 8, 15, 16, 23, 42 };
+//int total = 0;
 
 
-foreach (int number in numbers)
+//foreach (int number in numbers)
+//{
+//    total += number;
+
+//    if (number == 42)
+//    {
+//        Console.WriteLine("Set contains 42");
+
+//    }
+//}
+
+
+//Console.WriteLine($"Total: {total}");
+
+// SKU = Stock Keeping Unit. 
+// SKU value format: <product #>-<2-letter color code>-<size code>
+string sku = "01-MN-L";
+
+string[] product = sku.Split('-');
+
+string type = "";
+string color = "";
+string size = "";
+
+switch (product[0])
 {
-    total += number;
-
-    if (number == 42)
-    {
-        Console.WriteLine("Set contains 42");
-
-    }
+    case "01":
+        type = "Sweat shirt";
+        break;
+    case "02":
+        type = "T-Shirt";
+        break;
+    case "03":
+        type = "Sweat pants";
+        break;
+    default:
+        type = "other";
+        break;
 }
 
+switch (product[1])
+{
+    case "BL":
+        color = "Black";
+        break;
+    case "MN":
+        color = "Maroon";
+        break;
+    default:
+        color = "White";
+        break;
+}
 
-Console.WriteLine($"Total: {total}");
+switch (product[2])
+{
+    case "S":
+        size = "Small";
+        break;
+    case "M":
+        size = "Medium";
+        break;
+    case "L":
+        size = "Large";
+        break;
+    default:
+        size = "One Size Fits All";
+        break;
+}
+Console.WriteLine($"Product: {size} {color} {type}");
