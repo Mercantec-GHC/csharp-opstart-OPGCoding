aba238200fb4053ca0eefc8f887fd05e34883d0b
OPGCoding <97985921+OPGCoding@users.noreply.github.com>
2024-02-22 08:44:42 +0100

    Review a solution for combining string array values as strings and as integers challenge

--- Dag 4.1 - ConsoleApp/Program.cs
+++ Dag 4.1 - ConsoleApp/Program.cs
@@ -30,19 +30,41 @@
 
 
 
-string value = "102";
-int result = 0;
-if (int.TryParse(value, out result))
-{
-    Console.WriteLine($"Measurement: {result}");
-}
-else
-{
-    Console.WriteLine("Unable to report the measurement.");
+//string value = "102";
+//int result = 0;
+//if (int.TryParse(value, out result))
+//{
+//    Console.WriteLine($"Measurement: {result}");
+//}
+//else
+//{
+//    Console.WriteLine("Unable to report the measurement.");
 
-}
-if (result > 0)
-{
-    Console.WriteLine($"Measurement (w/ offset): {50 + result}");
+//}
+//if (result > 0)
+//{
+//    Console.WriteLine($"Measurement (w/ offset): {50 + result}");
 
+//}
+
+
+string[] values = { "12.3", "45", "ABC", "11", "DEF" };
+
+decimal total = 0m;
+string message = "";
+
+foreach (var value in values)
+{
+    decimal number; // stores the TryParse "out" value
+    if (decimal.TryParse(value, out number))
+    {
+        total += number;
+    }
+    else
+    {
+        message += value;
+    }
 }
+
+Console.WriteLine($"Message: {message}");
+Console.WriteLine($"Total: {total}");
