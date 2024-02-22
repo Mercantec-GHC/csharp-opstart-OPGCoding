d6b1d42e40a5a15741a70409ca5fbff9403fbd1d
OPGCoding <97985921+OPGCoding@users.noreply.github.com>
2024-02-22 09:01:22 +0100

    Array sort examples

--- Dag 4.1 - ConsoleApp/Program.cs
+++ Dag 4.1 - ConsoleApp/Program.cs
@@ -69,20 +69,39 @@
 //Console.WriteLine($"Message: {message}");
 //Console.WriteLine($"Total: {total}");
 
-int value1 = 12;
-decimal value2 = 6.2m;
-float value3 = 4.3f;
+//int value1 = 12;
+//decimal value2 = 6.2m;
+//float value3 = 4.3f;
 
-int result1 = Convert.ToInt32((decimal)value1 / value2);
-decimal result2 = value2 / (decimal)value3;
-float result3 = value3 / value1;
+//int result1 = Convert.ToInt32((decimal)value1 / value2);
+//decimal result2 = value2 / (decimal)value3;
+//float result3 = value3 / value1;
 
-// Your code here to set result1
-// Hint: You need to round the result to nearest integer (don't just truncate)
-Console.WriteLine($"Divide value1 by value2, display the result as an int: {result1}");
+//// Your code here to set result1
+//// Hint: You need to round the result to nearest integer (don't just truncate)
+//Console.WriteLine($"Divide value1 by value2, display the result as an int: {result1}");
 
-// Your code here to set result2
-Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
+//// Your code here to set result2
+//Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
 
-// Your code here to set result3
-Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
+//// Your code here to set result3
+//Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
+
+
+string[] pallets = { "B14", "A11", "B12", "A13" };
+
+Console.WriteLine("Sorted...");
+Array.Sort(pallets);
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
+
+Console.WriteLine("");
+Console.WriteLine("Reversed...");
+Array.Reverse(pallets);
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
+
