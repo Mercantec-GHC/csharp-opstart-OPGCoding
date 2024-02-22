e4dfd7a50f0fb2b6cb6150836459917dea62449d
OPGCoding <97985921+OPGCoding@users.noreply.github.com>
2024-02-22 12:36:42 +0100

    Review a solution to string interpolation

--- Dag 4.1 - ConsoleApp/Program.cs
+++ Dag 4.1 - ConsoleApp/Program.cs
@@ -249,21 +249,53 @@
 //Console.WriteLine($"\t\t\tTax: {taxPercentage:P2}");
 //Console.WriteLine($"\t\tTotal Billed: {total:C}");
 
-string input = "Pad this";
-Console.WriteLine(input.PadLeft(10, '-'));
-Console.WriteLine(input.PadLeft(24, '-'));
-Console.WriteLine(input.PadRight(12, '.'));
+//string input = "Pad this";
+//Console.WriteLine(input.PadLeft(10, '-'));
+//Console.WriteLine(input.PadLeft(24, '-'));
+//Console.WriteLine(input.PadRight(12, '.'));
 
-string paymentId = "769C";
-string payeeName = "Mr. Stephen Ortega";
-string paymentAmount = "$5,000.00";
+//string paymentId = "769C";
+//string payeeName = "Mr. Stephen Ortega";
+//string paymentAmount = "$5,000.00";
 
-var formattedLine = paymentId.PadRight(6);
-formattedLine += payeeName.PadRight(24);
-formattedLine += paymentAmount.PadLeft(10);
+//var formattedLine = paymentId.PadRight(6);
+//formattedLine += payeeName.PadRight(24);
+//formattedLine += paymentAmount.PadLeft(10);
 
-Console.WriteLine(formattedLine);
+//Console.WriteLine(formattedLine);
 
+
+
+string customerName = "Ms. Barros";
+
+string currentProduct = "Magic Yield";
+int currentShares = 2975000;
+decimal currentReturn = 0.1275m;
+decimal currentProfit = 55000000.0m;
+
+string newProduct = "Glorious Future";
+decimal newReturn = 0.13125m;
+decimal newProfit = 63000000.0m;
+
+Console.WriteLine($"Dear {customerName},");
+Console.WriteLine($"As a customer of our {currentProduct} offering we are excited to tell you about a new financial product that would dramatically increase your return.\n");
+Console.WriteLine($"Currently, you own {currentShares:N} shares at a return of {currentReturn:P}.\n");
+Console.WriteLine($"Our new product, {newProduct} offers a return of {newReturn:P}.  Given your current volume, your potential profit would be {newProfit:C}.\n");
+
+Console.WriteLine("Here's a quick comparison:\n");
+
+string comparisonMessage = "";
+
+comparisonMessage = currentProduct.PadRight(20);
+comparisonMessage += String.Format("{0:P}", currentReturn).PadRight(10);
+comparisonMessage += String.Format("{0:C}", currentProfit).PadRight(20);
+
+comparisonMessage += "\n";
+comparisonMessage += newProduct.PadRight(20);
+comparisonMessage += String.Format("{0:P}", newReturn).PadRight(10);
+comparisonMessage += String.Format("{0:C}", newProfit).PadRight(20);
+
+Console.WriteLine(comparisonMessage);
 
 
 
