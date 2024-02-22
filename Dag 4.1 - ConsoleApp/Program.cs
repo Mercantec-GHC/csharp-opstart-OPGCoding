5306d55bf66b07136ccc8addfcb9906e31c6ffe9
OPGCoding <97985921+OPGCoding@users.noreply.github.com>
2024-02-22 12:19:34 +0100

    Combining formatting approaches

--- Dag 4.1 - ConsoleApp/Program.cs
+++ Dag 4.1 - ConsoleApp/Program.cs
@@ -220,10 +220,21 @@
 //int discount = 50;
 //Console.WriteLine($"Price: {price:C} (Save {discount:C})");
 
-decimal measurement = 123456.78912345345324m;
-Console.WriteLine($"Measurement: {measurement:N9} units");
+//decimal measurement = 123456.78912345345324m;
+//Console.WriteLine($"Measurement: {measurement:N9} units");
 
 
+//decimal tax = .36785m;
+//Console.WriteLine($"Tax rate: {tax:P2}");
+
+
+decimal price = 67.55m;
+decimal salePrice = 59.99m;
+
+string yourDiscount = String.Format("You saved {0:C2} off the regular {1:C2} price. ", (price - salePrice), price);
+
+yourDiscount += $"A discount of {((price - salePrice) / price):P2}!"; //inserted
+Console.WriteLine(yourDiscount);
 
 
 
