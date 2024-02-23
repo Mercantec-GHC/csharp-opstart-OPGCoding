2b3002e9380902acd8268e52902811faa05ce766
OPGCoding <97985921+OPGCoding@users.noreply.github.com>
2024-02-23 11:00:38 +0100

    Exercise - Complete the challenge to add methods to make the game playable

--- Dag 5.1 - ConsoleApp/Program.cs
+++ Dag 5.1 - ConsoleApp/Program.cs
@@ -289,53 +289,102 @@
 //}
 
 
-int target = 30;
-int[] coins = new int[] { 5, 5, 50, 25, 25, 10, 5 };
-int[,] result = TwoCoins(coins, target);
+//int target = 30;
+//int[] coins = new int[] { 5, 5, 50, 25, 25, 10, 5 };
+//int[,] result = TwoCoins(coins, target);
 
-if (result.Length == 0)
+//if (result.Length == 0)
+//{
+//	Console.WriteLine("No two coins make change");
+//}
+//else
+//{
+//	Console.WriteLine("Change found at positions:");
+//	for (int i = 0; i < result.GetLength(0); i++)
+//	{
+//		if (result[i, 0] == -1)
+//		{
+//			break;
+//		}
+//		Console.WriteLine($"{result[i, 0]},{result[i, 1]}");
+//	}
+//}
+
+//int[,] TwoCoins(int[] coins, int target)
+//{
+//	int[,] result = { { -1, -1 }, { -1, -1 }, { -1, -1 }, { -1, -1 }, { -1, -1 } };
+//	int count = 0;
+
+//	for (int curr = 0; curr < coins.Length; curr++)
+//	{
+//		for (int next = curr + 1; next < coins.Length; next++)
+//		{
+//			if (coins[curr] + coins[next] == target)
+//			{
+//				result[count, 0] = curr;
+//				result[count, 1] = next;
+//				count++;
+//			}
+//			if (count == result.GetLength(0))
+//			{
+//				return result;
+//			}
+//		}
+//	}
+//	return (count == 0) ? new int[0, 0] : result;
+//}
+
+
+Random random = new Random();
+
+Console.WriteLine("Would you like to play? (Y/N)");
+if (ShouldPlay())
 {
-	Console.WriteLine("No two coins make change");
+	PlayGame();
 }
-else
+
+bool ShouldPlay()
 {
-	Console.WriteLine("Change found at positions:");
-	for (int i = 0; i < result.GetLength(0); i++)
+	string response = Console.ReadLine();
+	return response.ToLower().Equals("y");
+}
+
+void PlayGame()
+{
+	var play = true;
+
+	while (play)
 	{
-		if (result[i, 0] == -1)
-		{
-			break;
-		}
-		Console.WriteLine($"{result[i, 0]},{result[i, 1]}");
+		var target = GetTarget();
+		var roll = RollDice();
+
+		Console.WriteLine($"Roll a number greater than {target} to win!");
+		Console.WriteLine($"You rolled a {roll}");
+		Console.WriteLine(WinOrLose(roll, target));
+		Console.WriteLine("\nPlay again? (Y/N)");
+
+		play = ShouldPlay();
 	}
 }
 
-int[,] TwoCoins(int[] coins, int target)
+int GetTarget()
 {
-	int[,] result = { { -1, -1 }, { -1, -1 }, { -1, -1 }, { -1, -1 }, { -1, -1 } };
-	int count = 0;
-
-	for (int curr = 0; curr < coins.Length; curr++)
-	{
-		for (int next = curr + 1; next < coins.Length; next++)
-		{
-			if (coins[curr] + coins[next] == target)
-			{
-				result[count, 0] = curr;
-				result[count, 1] = next;
-				count++;
-			}
-			if (count == result.GetLength(0))
-			{
-				return result;
-			}
-		}
-	}
-	return (count == 0) ? new int[0, 0] : result;
+	return random.Next(1, 6);
 }
 
+int RollDice()
+{
+	return random.Next(1, 7);
+}
 
-
+string WinOrLose(int roll, int target)
+{
+	if (roll > target)
+	{
+		return "You win!";
+	}
+	return "You lose!";
+}
 
 
 
