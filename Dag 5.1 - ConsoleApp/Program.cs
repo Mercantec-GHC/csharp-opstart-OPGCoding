6070eee34d215bc220484a6a45008d03b1c6ec9b
OPGCoding <97985921+OPGCoding@users.noreply.github.com>
2024-02-23 10:59:37 +0100

    Exercise - Return arrays from methods

--- Dag 5.1 - ConsoleApp/Program.cs
+++ Dag 5.1 - ConsoleApp/Program.cs
@@ -262,29 +262,77 @@
 //}
 
 
-double total = 0;
-double minimumSpend = 30.00;
+//string input = "there are snakes at the zoo";
 
-double[] items = { 15.97, 3.50, 12.25, 22.99, 10.98 };
-double[] discounts = { 0.30, 0.00, 0.10, 0.20, 0.50 };
+//Console.WriteLine(input);
+//Console.WriteLine(ReverseSentence(input));
 
-Console.WriteLine($"Total: ${total}");
+//string ReverseSentence(string input)
+//{
+//	string result = "";
+//	string[] words = input.Split(" ");
+//	foreach (string word in words)
+//	{
+//		result += ReverseWord(word) + " ";
+//	}
+//	return result.Trim();
+//}
 
-void GetDiscountedPrice(int itemIndex)
+//string ReverseWord(string word)
+//{
+//	string result = "";
+//	for (int i = word.Length - 1; i >= 0; i--)
+//	{
+//		result += word[i];
+//	}
+//	return result;
+//}
+
+
+int target = 30;
+int[] coins = new int[] { 5, 5, 50, 25, 25, 10, 5 };
+int[,] result = TwoCoins(coins, target);
+
+if (result.Length == 0)
 {
-	// Calculate the discounted price of the item
+	Console.WriteLine("No two coins make change");
+}
+else
+{
+	Console.WriteLine("Change found at positions:");
+	for (int i = 0; i < result.GetLength(0); i++)
+	{
+		if (result[i, 0] == -1)
+		{
+			break;
+		}
+		Console.WriteLine($"{result[i, 0]},{result[i, 1]}");
+	}
 }
 
-void TotalMeetsMinimum()
+int[,] TwoCoins(int[] coins, int target)
 {
-	// Check if the total meets the minimum
-}
+	int[,] result = { { -1, -1 }, { -1, -1 }, { -1, -1 }, { -1, -1 }, { -1, -1 } };
+	int count = 0;
 
-void FormatDecimal(double input)
-{
-	// Format the double so only 2 decimal places are displayed
+	for (int curr = 0; curr < coins.Length; curr++)
+	{
+		for (int next = curr + 1; next < coins.Length; next++)
+		{
+			if (coins[curr] + coins[next] == target)
+			{
+				result[count, 0] = curr;
+				result[count, 1] = next;
+				count++;
+			}
+			if (count == result.GetLength(0))
+			{
+				return result;
+			}
+		}
+	}
+	return (count == 0) ? new int[0, 0] : result;
 }
-
 
 
 
